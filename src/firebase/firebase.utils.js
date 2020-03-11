973b59282caf00dca571509eb5b5ba8b55b5b8c4
Caio <caioymc@live.com>
2020-03-11 13:52:10 +0000

    Added sign in/up page, setup Firebase, create sign in form and sign in with Google acc

---
+++ src/firebase/firebase.utils.js
@@ -0,0 +1,24 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
+
+const config = {
+  apiKey: "AIzaSyCthQmDXOusWnl7_jG7umIsemyzCGDJELg",
+  authDomain: "market-db-b72b6.firebaseapp.com",
+  databaseURL: "https://market-db-b72b6.firebaseio.com",
+  projectId: "market-db-b72b6",
+  storageBucket: "market-db-b72b6.appspot.com",
+  messagingSenderId: "355859918552",
+  appId: "1:355859918552:web:ba7faf5462c4ac567af1ad"
+};
+
+firebase.initializeApp(config);
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+const provider = new firebase.auth.GoogleAuthProvider();
+provider.setCustomParameters({prompt: 'select_account'});
+export const signInWithGoogle = () => auth.signInWithPopup(provider);
+
+export default firebase;
