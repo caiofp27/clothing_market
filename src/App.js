7fff63016845ea75b042d8d7c068f786854b9d46
Caio <caioymc@live.com>
2020-03-25 11:30:25 +0000

    Updated app to redirect to home if user signed in

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {Switch, Route} from 'react-router-dom';
+import {Switch, Route, Redirect} from 'react-router-dom';
 import {auth, createUserProfileDocument} from './firebase/firebase.utils';
 import {connect} from 'react-redux';
 import {setCurrentUser} from './redux/user/user.actions';
 import './App.css';
+import userReducer from './redux/user/user.reducer';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -40,15 +41,23 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
-          <Route path='/signin' component={SignInAndSignUpPage} />
+          <Route exact path='/signin' render={() => 
+            this.props.currentUser ? 
+              (<Redirect to='/' />) : 
+              (<SignInAndSignUpPage />)
+            } />
         </Switch>
       </div>
     );
   }
 }
 
+const mapStateProps = ({user}) => ({
+  currentUser: user.currentUser
+});
+
 const mapDispatchProps = dispatch => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
 });
 
-export default connect(null, mapDispatchProps)(App);
+export default connect(mapStateProps, mapDispatchProps)(App);
